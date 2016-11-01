53ff89b9f71b04f7d277fe4979ac71c2c479bbf9
Ben Larrabee <ben.larrabee@interapthq.com>
2016-11-01 16:56:28 -0400

    Tue 4:56 storyboard begun

---
+++ EFAB/ViewControllers/BudgetViewController.swift
@@ -0,0 +1,37 @@
+//
+//  BudgetViewController.swift
+//  EFAB
+//
+//  Created by Ben Larrabee on 11/1/16.
+//  Copyright © 2016 Ben Larrabee. All rights reserved.
+//
+
+import UIKit
+
+class BudgetViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    if !WebServices.shared.userAuthTokenExists() || WebServices.shared.userAuthTokenExpired() {
+      performSegue(withIdentifier: "PresentLoginNoAnimation", sender: self)      
+    }
+  }
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+}
