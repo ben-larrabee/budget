f8b65a134cce3f17192b419e46e714b99d93dd17
Ben Larrabee <ben.larrabee@interapthq.com>
2016-11-02 10:58:33 -0400

    Wed 10:58 working on User authorization

---
+++ EFAB/Components/UserStore.swift
@@ -0,0 +1,27 @@
+//
+//  UserStore.swift
+//  EFAB
+//
+//  Created by Ben Larrabee on 11/2/16.
+//  Copyright © 2016 Ben Larrabee. All rights reserved.
+//
+
+import Foundation
+
+class UserStore {
+  // singleton
+  static let shared = UserStore()
+  private init() {}
+  
+  func login(_ loginUser: User, completion: @escaping (_ success: Bool, _ error: String?) -> Void) {
+    // Call web service to login
+    WebServices.shared.authUser(loginUser) { (user, error) in
+      if let user = user {
+        WebServices.shared.setAuthToken(user.token, expiration: user.expiration)
+        completion(true, nil)
+      } else {
+        completion(false, error)
+      }
+    }
+  }
+}
